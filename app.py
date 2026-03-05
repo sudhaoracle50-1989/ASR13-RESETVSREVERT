b00a9d11bcf2d1f78e19648b24d67995e0c7b7ea
sudhakar <sudhaoracle50@gmail.com>
2026-03-05 11:11:16 +0530

    test

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # Simple Python program
 
-print("Hello, World!")
+print("Hello, World! how are you")
 
+<<<<<<< HEAD
 name = input("sudhakar reddy ")
 print("Dear darling", name)
+=======
+name = input("Enter your name: ")
+print("Hello", name)
+>>>>>>> parent of 360fbd2 (added second update name sudhakar reddy)
